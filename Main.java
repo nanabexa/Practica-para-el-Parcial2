c0ddee04b54e1e0e48b8169171f1d90cc62ad837
Keniak1-G9 <84326221+Keniak1-G9@users.noreply.github.com>
2025-06-05 09:30:44 -0500

    Agregado parte de la funcionalidad a Main
    - Se agregó las variables
    - Se agregó un bloque try-catch simple

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main
@@ -7,6 +8,43 @@ public class Main
     {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("");
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String nombre;
+        String apellido;
+        String sexo;
+        double salario;
+        double montoDelPrestamo;
+
+        System.out.println("Solicitud de Prestamo");
+
+        try
+        {
+            System.out.println("Introduzca el nombre del cliente");
+            nombre = bufferedReader.readLine();
+
+            System.out.println("Introduzca el apellido del cliente");
+            apellido = bufferedReader.readLine();
+
+            System.out.println("Introduzca el sexo del cliente");
+            sexo = bufferedReader.readLine();
+
+            System.out.println("Introduzca el apellido del cliente");
+            salario = Double.parseDouble(bufferedReader.readLine());
+
+            System.out.println("Introduzca el monto de del prestamo");
+            montoDelPrestamo = Double.parseDouble(bufferedReader.readLine());
+
+        } catch (NumberFormatException e)
+        {
+            System.out.println("El dato introducido no es un número");
+        } catch (IOException e)
+        {
+            System.out.println("Hubo algun tipo de error");
+        }
+
+        // A futuro cuando este:
+        // Cliente cliente = new Cliente(nombre, apellido, sexo, salario, montoDelPrestamo);
 
         System.out.println("Introduzca el nombre del cliente");
         System.out.println("Introduzca el apellido del cliente");
