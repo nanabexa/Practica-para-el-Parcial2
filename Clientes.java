f4abb10fdf89fcfe6f47eda1ad08e6adb5c7ac88
Jotope <thiagovaleirao@gmail.com>
2025-06-05 09:10:09 -0500

    Primer balatro que balatreo2

--- Clientes.java
+++ Clientes.java
@@ -1,2 +1,52 @@
 public class Clientes {
+    String nombreCliente;
+    String sexoCliente;
+    double salarioCliente;
+    double montoPrestamoCliente;
+
+    public Clientes() {
+        this.nombreCliente = "";
+        this.sexoCliente = "";
+        this.salarioCliente = 0.0;
+        this.montoPrestamoCliente = 0.0;
+    }
+
+    //Metodos getters
+    public String getNombreCliente() {
+        return nombreCliente;
+    }
+    public String getSexoCliente() {
+        return sexoCliente;
+    }
+    public double getSalarioCliente() {
+        return salarioCliente;
+    }
+    public double getMontoPrestamoCliente() {
+        return montoPrestamoCliente;
+    }
+
+    //Metodos setters
+    public void setNombreCliente(String nombreCliente) {
+        this.nombreCliente = nombreCliente;
+    }
+    public void setSexoCliente(String sexoCliente) {
+        this.sexoCliente = sexoCliente;
+    }
+    public void setSalarioCliente(double salarioCliente) {
+        this.salarioCliente = salarioCliente;
+    }
+    public void setMontoPrestamoCliente(double montoPrestamoCliente) {
+        this.montoPrestamoCliente = montoPrestamoCliente;
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
